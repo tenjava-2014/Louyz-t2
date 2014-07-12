f5afecac3bf41bc5e530515deedf79ea8488d5fb
Luis Tavares <luysdmk.77@gmail.com>
2014-07-12 13:07:18 +0100

    Add a way to remove Cables and a Shaped Recipe

---
+++ src/main/java/com/tenjava/entries/Louyz/t2/cable/CableManager.java
@@ -0,0 +1,7 @@
+package com.tenjava.entries.Louyz.t2.cable;
+
+/**
+ * Created by Luis on 12/07/2014.
+ */
+public class CableManager {
+}
---
+++ src/main/java/com/tenjava/entries/Louyz/t2/events/InteractEvent.java
@@ -0,0 +1,7 @@
+package com.tenjava.entries.Louyz.t2.events;
+
+/**
+ * Created by Luis on 12/07/2014.
+ */
+public class InteractEvent {
+}
